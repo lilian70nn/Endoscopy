70f47b77be1ed3b7317566c4a901e386a22eed9a
lilian70nn <lilian70nn@gmail.com>
2026-09-16 20:19:23 +0200

    update

--- train_1.py
+++ train_1.py
@@ -15,7 +15,7 @@ from lejepa import train_lejepa
 
 
 def main():
-    dataloader = initialize_dataloader(batch_size=400)
+    dataloader = initialize_dataloader(batch_size=256)
 
     experiments = [
         ("dino", "siglip2"),
